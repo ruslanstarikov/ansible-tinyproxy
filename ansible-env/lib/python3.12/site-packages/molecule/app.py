b2386162f02604f9b270b5c97877598475842605
Ruslan Starikov <ruslan@beef-burger.com>
2024-04-22 11:46:53 +1000

    Tests are working on MacOS

---
+++ ansible-env/lib/python3.12/site-packages/molecule/app.py
@@ -0,0 +1,14 @@
+"""Molecule Application Module."""
+
+from ansible_compat.runtime import Runtime
+
+
+class App:
+    """App class that keep runtime status."""
+
+    def __init__(self) -> None:
+        """Create a new app instance."""
+        self.runtime = Runtime(isolated=False)
+
+
+app = App()
